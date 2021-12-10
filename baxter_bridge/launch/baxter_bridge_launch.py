63f07fa9475b8e93afeaadb1d6fd878883b58922
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2021-12-10 17:04:15 +0100

    typo in launch file

--- baxter_bridge/launch/baxter_bridge_launch.py
+++ baxter_bridge/launch/baxter_bridge_launch.py
@@ -2,7 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription,  GroupAction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.conditions import IfCondition
