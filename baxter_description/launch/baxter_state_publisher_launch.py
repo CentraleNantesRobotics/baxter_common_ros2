5133604e2da5914fcb329ad0dc5253027e532a2e
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2022-11-21 15:46:32 +0100

    uses /robot/robot_description

--- baxter_description/launch/baxter_state_publisher_launch.py
+++ baxter_description/launch/baxter_state_publisher_launch.py
@@ -2,9 +2,9 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument,GroupAction
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import Node, PushRosNamespace
 import xacro
 
 def generate_launch_description():
@@ -14,12 +14,10 @@ def generate_launch_description():
         'urdf',
         'baxter.urdf.xacro')
 
-    return LaunchDescription([
-        Node(
-            package='robot_state_publisher',
+    rsp = Node(package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description': xacro.process(urdf)}],
-            remappings={'/joint_states': '/robot/joint_states'}.items()),
-    ])
+            parameters=[{'robot_description': xacro.process(urdf)}])
+
+    return LaunchDescription([GroupAction([PushRosNamespace('robot'), rsp])])
